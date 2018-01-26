0f6aaaa59b0a9e6bf9ed068903d94ea79e61913d
“dsoper2” <dsoper@cisco.com>
2018-01-26 17:35:33 -0600

    Simplify args for claim and delete device.  Proxy support for claim.

--- examples/delete_device.py
+++ examples/delete_device.py
@@ -14,34 +14,30 @@ if __name__ == "__main__":
     try:
         # settings are pulled from the json string or JSON file passed as an arg
         parser = argparse.ArgumentParser()
-        parser.add_argument('json_arg', help='JSON settings file or JSON object as a string')
+        parser.add_argument('-f', '--file', default='settings.json', help='JSON settings file (settings.json used by default)')
+        parser.add_argument('-t', '--target_host', dest='hostname', help='target host ip to delete')
         args = parser.parse_args()
-        if os.path.isfile(args.json_arg):
-            settings = json.load(open(args.json_arg, 'r'))
+        if os.path.isfile(args.file):
+            settings = json.load(open(args.file, 'r'))
         else:
-            settings = json.loads(args.json_arg)
+            settings = json.loads(args.file)
 
-        if settings.get('hosts'):
-            hosts_list = settings['hosts']
-        else:
-            hosts_list = [settings]
-        for host in hosts_list:
-            # Create Intersight API instance
-            # ----------------------
-            api_instance = IntersightApiClient(host=settings['api_host'], private_key=settings['api_private_key'], api_key_id=settings['api_key_id'])
+        # Create Intersight API instance
+        # ----------------------
+        api_instance = IntersightApiClient(host=settings['api_host'], private_key=settings['api_private_key'], api_key_id=settings['api_key_id'])
 
-            # create API handle
-            api_handle = asset_device_registration_api.AssetDeviceRegistrationApi(api_instance)
+        # create API handle
+        api_handle = asset_device_registration_api.AssetDeviceRegistrationApi(api_instance)
 
-            kwargs = dict(filter="ConnectionStatus eq 'Connected'")
-            api_result = api_handle.asset_device_registrations_get(**kwargs)
-            for device in api_result.results:
-                if device.device_ip_address[0] == host['hostname']:
-                    api_handle = asset_device_claim_api.AssetDeviceClaimApi(api_instance)
-                    api_handle.asset_device_claims_moid_delete(moid=device.device_claim.moid)
-                    result['changed'] = True
+        kwargs = dict(filter="ConnectionStatus eq 'Connected'")
+        api_result = api_handle.asset_device_registrations_get(**kwargs)
+        for device in api_result.results:
+            if device.device_ip_address[0] == args.hostname:
+                api_handle = asset_device_claim_api.AssetDeviceClaimApi(api_instance)
+                api_handle.asset_device_claims_moid_delete(moid=device.device_claim.moid)
+                result['changed'] = True
 
-            print json.dumps(result)
+        print json.dumps(result)
 
     except Exception, err:
         print "Exception:", str(err)
